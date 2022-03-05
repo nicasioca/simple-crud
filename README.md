cda604f84c15894f102c72648d9e9f0ea5161195
Nick Anderson <nicasioca@gmail.com>
2022-03-05 14:34:50 -0800

    complete intro and insert steps

---
+++ README.md
@@ -0,0 +1,3 @@
+# Simple CRUD example with Sea-ORM
+Follow the steps in the tutorial below.
+https://www.sea-ql.org/sea-orm-tutorial/introduction.html
---
+++ src/fruits_table/prelude.rs
@@ -0,0 +1,4 @@
+pub use super::fruits::{
+  ActiveModel as FruitsActiveModel, Column as FruitsColumn, Entity as Fruits,
+  Model as FruitsModel, PrimaryKey as FruitsPrimaryKey, Relation as FruitsRelation,
+};
