fc15fd9b206746cba0f0622c23228f1db732035a
Nick Anderson <nicasioca@gmail.com>
2022-03-06 15:15:36 -0800

    finish tutorial of sea-orm

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,7 @@
 mod fruits_table;
 use fruits_table::prelude::*;
+mod suppliers_table;
+use suppliers_table::prelude::*;
 
 // Import the needed modules for table creation
 use sea_orm::{entity::Set, prelude::*, ConnectionTrait, Database, Schema};
@@ -62,6 +64,72 @@ async fn main() -> Result<()>{
   };
   let fruit_insert_operation = Fruits::insert_many(vec![fruit_02, fruit_03, fruit_04]).exec(&db).await;
   println!("INSERTED MANY: {:?}", fruit_insert_operation?);
+
+  let fruits_table_rows = Fruits::find().all(&db).await;
+  println!("{:?}", fruits_table_rows?);
+
+  let fruits_by_id = Fruits::find_by_id(2).one(&db).await;
+  println!("{:?}", fruits_by_id?);
+
+  let find_pineapple = Fruits::find()
+      .filter(FruitsColumn::Name.contains("pineapple"))
+      .one(&db)
+      .await?;
+  println!("{:?}", find_pineapple.as_ref());
+
+  // Update the `pineapple` column with a new unit price
+  if let Some(pineapple_model) = find_pineapple {
+      let mut pineapple_active_model: FruitsActiveModel = pineapple_model.into();
+      pineapple_active_model.unit_price = Set(10);
+
+      let updated_pineapple_model: FruitsModel = pineapple_active_model.update(&db).await?;
+
+      println!("UPDATED PRICE: {:?}", updated_pineapple_model.clone());
+  } else {
+      println!("`Pineapple` column not found");
+  }
+
+  // Delete the `mango` column
+
+  let find_mango = Fruits::find()
+      .filter(FruitsColumn::Name.contains("mango"))
+      .one(&db)
+      .await;
+
+  if let Some(mango_model) = find_mango? {
+      println!("DELETED MANGO: {:?}", mango_model.delete(&db).await?);
+  } else {
+      println!("`Mango` column not found");
+  }
+
+  let supplier_01 = SuppliersActiveModel {
+    supplier_name: Set("John Doe".to_owned()),
+    fruit_id: Set(1_i32),
+    ..Default::default()
+  };
+
+  let supplier_02 = SuppliersActiveModel {
+      supplier_name: Set("Jane Doe".to_owned()),
+      fruit_id: Set(2_i32),
+      ..Default::default()
+  };
+
+  let supplier_03 = SuppliersActiveModel {
+      supplier_name: Set("Junior Doe".to_owned()),
+      fruit_id: Set(3_i32),
+      ..Default::default()
+  };
+
+  let supplier_insert_operation =
+      Suppliers::insert_many(vec![supplier_01, supplier_02, supplier_03])
+          .exec(&db)
+          .await;
+
+  println!("INSERTED MANY: {:?}", supplier_insert_operation?);
+
+  let who_supplies = Suppliers::find().find_with_related(Fruits).all(&db).await?;
+
+  dbg!(&who_supplies);
 
   Ok(())
 }
